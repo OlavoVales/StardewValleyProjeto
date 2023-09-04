cffc27ce0118bfe1351de65c809d10525761fd12
OlavoVales <olavovgcs@gmail.com>
2023-09-03 22:14:05 -0300

    correção da sintaxe no código do component de imagens

--- src/components/Armas/DadosArmas/ImagensComponent/ImagensComponent.jsx
+++ src/components/Armas/DadosArmas/ImagensComponent/ImagensComponent.jsx
@@ -2,7 +2,7 @@ import styles from './ImagensComponent.module.css'
 
 function ImagensComponent({ item }) {
     return (
-        <h3><img src={`/imgs/armas/${item.tipo-item}/${item.nome}.png`}/></h3>
+        <h3><img src={`/imgs/armas/${item.tipoitem}/${item.nome}.png`}/></h3>
     )
 }
 
--- src/components/FerramentasBase/DadosFerramentasBase/ImagensComponent/ImagensComponent.jsx
+++ src/components/FerramentasBase/DadosFerramentasBase/ImagensComponent/ImagensComponent.jsx
@@ -2,7 +2,7 @@ import styles from './ImagensComponent.module.css'
 
 function ImagensComponent({ item }) {
     return (
-        <h3><img src={`/imgs/ferramentas/${item.tipo-item}/${item.nome}.png`}/></h3>
+        <h3><img src={`/imgs/ferramentas/${item.tipoitem}/${item.nome}.png`}/></h3>
     )
 }
 
