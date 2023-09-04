57607a6752697cdebf17b509515f52136bd0f92c
OlavoVales <olavovgcs@gmail.com>
2023-09-03 22:21:00 -0300

    correção da sintaxe no código do component de imagens

--- src/components/VarasDePesca/DadosVarasDePesca/ImagensComponent/ImagensComponent.jsx
+++ src/components/VarasDePesca/DadosVarasDePesca/ImagensComponent/ImagensComponent.jsx
@@ -3,7 +3,7 @@ import styles from './ImagensComponent.module.css'
 function ImagensComponent({ item }) {
     return (
         <h3><img src={`/imgs/ferramentas/${item.tipoitem}/${item.nome}.png`}/></h3>
-    )
+    )   /* seria mais esquema ter adicionado um elemento imagem no .json com o URL da imagem do que ter feito assim, mas agr que já fiz deixa assim */
 }
 
 export default ImagensComponent
